be92f7e455c4e35f081774c359c3c484eaae5fe3
Arthemyst <pawel.lukmin@gmail.com>
2023-06-03 15:30:55 +0200

    Created game algorithm with negamax for ai player.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+from tic_tac_toe_game.player import HumanPlayer, AIPlayer
+from tic_tac_toe_game.tic_tac_toe import TicTacToe
+from tic_tac_toe_game.ai.negamax import Negamax
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
@@ -6,6 +10,29 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+ai_algo = Negamax(2)
+
+@app.route("/game", methods=["GET", "POST"])
+def play_game():
+    game = TicTacToe([HumanPlayer(), AIPlayer(ai_algo)])
+    game_cookie = request.cookies.get("game_board")
+    if game_cookie:
+        game.board = [int(x) for x in game_cookie.split(",")]
+    if "choice" in request.form:
+        game.play_move(request.form["choice"])
+        if not game.is_over():
+            ai_move = game.get_move()
+            game.play_move(ai_move)
+    if "reset" in request.form:
+        game.board = [0 for i in range(9)]
+    if game.is_over():
+        msg = game.winner()
+    else:
+        msg = "play move"
+    resp = make_response(render_template('game.html', game=game, msg=msg))
+    c = ",".join(map(str, game.board))
+    resp.set_cookie("game_board", c)
+    return resp
 
 def run_web_app():
     app.run(debug=True, host='0.0.0.0')
---
+++ src/tic_tac_toe_game/tic_tac_toe.py
@@ -0,0 +1,61 @@
+from tic_tac_toe_game.two_player_game import TwoPlayerGame
+from typing import List
+from tic_tac_toe_game.constants import WIN_LINES
+class TicTacToe(TwoPlayerGame):
+
+    def __init__(self, players):
+        self.players: List = players
+        self.board: List = [0 for i in range(9)]
+        self.current_player: int = 1
+
+    def possible_moves(self):
+        return [i + 1 for i, e in enumerate(self.board) if e == 0]
+
+    def make_move(self, move):
+        self.board[int(move) - 1] = self.current_player
+
+    def unmake_move(self, move):  # optional method (speeds up the AI)
+        self.board[int(move) - 1] = 0
+
+    def lose(self, who=None):
+        """ Has the opponent "three in line ?" """
+        if who is None:
+            who = self.opponent_index
+        wins = [all([(self.board[c - 1] == who) for c in line]) for line in WIN_LINES]
+        return any(wins)
+
+    def is_over(self):
+        return (
+            (self.possible_moves() == [])
+            or self.lose()
+            or self.lose(who=self.current_player)
+        )
+
+    def show(self):
+        print(
+            "\n"
+            + "\n".join(
+                [
+                    " ".join([[".", "O", "X"][self.board[3 * j + i]] for i in range(3)])
+                    for j in range(3)
+                ]
+            )
+        )
+
+    def spot_string(self, i, j):
+        return ["_", "O", "X"][self.board[3 * j + i]]
+
+    def scoring(self):
+        opp_won = self.lose()
+        i_won = self.lose(who=self.current_player)
+        if opp_won and not i_won:
+            return -100
+        if i_won and not opp_won:
+            return 100
+        return 0
+
+    def winner(self):
+        if self.lose(who=2):
+            return "AI Wins"
+        return "Tie"
+    
